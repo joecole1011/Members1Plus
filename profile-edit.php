e95e86572c7caaaf3188b9e43edd4b69dc5c8ec0
joecole1011 <akawit_7@hotmail.com>
2024-12-05 15:56:28 +0700

    first commit

--- profile-edit.php
+++ profile-edit.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css ">
     <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.min.css ">
 
-    <title>ประวัติส่วนตัว</title>
+    <title>แก้ไขประวัติส่วนตัว</title>
     <style>
         .img-profile {
             width: 150px;
@@ -39,29 +39,55 @@
         <div class="row">
             <div class="col-12 profile-top">
                 <img src="assets/image/avatar.png" class=" my-3 img-profile rounded-circle img-thumbnail" alt="Image Profile">
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                    Launch demo modal
+                </button>
+
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                ...
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <div class="card">
                     <div class="card-body">
                         <div class="form-row">
                             <div class="form-group col-md-6">
                                 <label for="username">ชื่อผู้ใช้งาน</label>
-                                <input type="text" class="form-control" id="username" value="Admin" disabled>
+                                <input type="text" class="form-control" id="username" value="Admin">
                             </div>
                             <div class="form-group col-md-6">
                                 <label for="name">ชื่อ-สกุล</label>
-                                <input type="text" class="form-control" id="name" value="Admin Manager" disabled>
+                                <input type="text" class="form-control" id="name" value="Admin Manager">
                             </div>
                             <div class="form-group col-md-6">
                                 <label for="email">อีเมลล์</label>
-                                <input type="email" class="form-control" id="email" value="aaa@example.com" disabled>
+                                <input type="email" class="form-control" id="email" value="aaa@example.com">
                             </div>
                             <div class="form-group col-md-6">
                                 <label for="phone">เบอร์โทรศัพท์</label>
-                                <input type="text" class="form-control" id="phone" disabled value="123456789">
+                                <input type="text" class="form-control" id="phone" value="123456789">
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="address">ที่อยู่</label>
-                            <textarea name="" id="address" class="form-control" rows="5" disabled>เพชรบูรณ์ เมืองเพชรบูรณ์
+                            <textarea name="" id="address" class="form-control" rows="5">
 
                             </textarea>
                         </div>
